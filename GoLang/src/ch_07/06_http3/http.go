9ae83fee41066e6f0851e05a1a0f739d4423c719
Leuckart <61966372@qq.com>
2021-03-02 21:05:17 +0800

    update_2021.03.02

--- GoLang/src/ch_07/06_http3/http.go
+++ GoLang/src/ch_07/06_http3/http.go
@@ -1,7 +1,7 @@
 /**************************************************
 	> File Name:  http.go
 	> Author:     Leuckart
-	> Time:       2021-01-25 04:33
+	> Time:       2021-01-25 04:38
 **************************************************/
 
 package main
@@ -14,33 +14,38 @@ import (
 
 type dollars float32
 
+type database map[string]dollars
+
 func (d dollars) String() string {
 	return fmt.Sprintf("$%.2f", d)
 }
 
-type database map[string]dollars
-
 func (db database) list(w http.ResponseWriter, req *http.Request) {
 	for item, price := range db {
-		fmt.Fprintf(w, "%s: %s\n", item, price)
+		fmt.Fprintf(w, "%s: $%s\n", item, price)
 	}
 }
 
 func (db database) price(w http.ResponseWriter, req *http.Request) {
 	item := req.URL.Query().Get("item")
-	price, ok := db[item]
-	if !ok {
+	if price, ok := db[item]; ok {
+		fmt.Fprintf(w, "%s\n", price)
+	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "no such item: %q\n", item)
-		return
 	}
-	fmt.Fprintf(w, "%s\n", price)
 }
 
+// ./http
+// ../../ch_01/05_fetch/fetch http://localhost:8000/list
+// ../../ch_01/05_fetch/fetch http://localhost:8000/price?item=shoes
+// ../../ch_01/05_fetch/fetch http://localhost:8000/price?item=socks
 func main() {
 	db := database{"shoes": 50, "socks": 5}
 	mux := http.NewServeMux()
-	mux.Handle("/list", http.HandlerFunc(db.list))
-	mux.Handle("/price", http.HandlerFunc(db.price))
+	// mux.Handle("/list", http.HandlerFunc(db.list))
+	// mux.Handle("/price", http.HandlerFunc(db.price))
+	mux.HandleFunc("/list", db.list)
+	mux.HandleFunc("/price", db.price)
 	log.Fatal(http.ListenAndServe("localhost:8000", mux))
 }
