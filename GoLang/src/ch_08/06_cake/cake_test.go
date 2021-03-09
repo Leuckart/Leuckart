bf9048b3f2d5f4ebca53fd9107b9bfeebc85fb6d
Leuckart <61966372@qq.com>
2021-03-09 14:57:07 +0800

    update_2021.03.07

--- GoLang/src/ch_08/06_cake/cake_test.go
+++ GoLang/src/ch_08/06_cake/cake_test.go
@@ -4,33 +4,15 @@
 	> Time:       2021-01-26 01:33
 **************************************************/
 
-// go test -bench=.
+// go test -v -bench=.
 package cake
 
 import (
 	"testing"
 	"time"
-	"fmt"
 )
 
-// https://github.com/golang/go/issues/31859
-func init()  {
-	testing.Init()
-}
-
-var _ = func() bool {
-	fmt.Println("1")
-	testing.Init()
-	fmt.Println("2")
-	fmt.Println(testing.Verbose())
-	return true
-}()
-
-
-
 var defaults = Shop{
-	Verbose: testing.Verbose(),
-	//Verbose:      false,
 	Cakes:        20,
 	BakeTime:     10 * time.Millisecond,
 	NumIcers:     1,
@@ -40,11 +22,13 @@ var defaults = Shop{
 
 func Benchmark(b *testing.B) {
 	cakeshop := defaults
+	cakeshop.Verbose = testing.Verbose()
 	cakeshop.Work(b.N) // 224ms
 }
 
 func BenchmarkBuffers(b *testing.B) {
 	cakeshop := defaults
+	cakeshop.Verbose = testing.Verbose()
 	cakeshop.BakeBuf = 10
 	cakeshop.IceBuf = 10
 	cakeshop.Work(b.N)
@@ -52,6 +36,7 @@ func BenchmarkBuffers(b *testing.B) {
 
 func BenchmarkVariable(b *testing.B) {
 	cakeshop := defaults
+	cakeshop.Verbose = testing.Verbose()
 	cakeshop.BakeStdDev = cakeshop.BakeTime / 4
 	cakeshop.IceStdDev = cakeshop.IceTime / 4
 	cakeshop.InscribeStdDev = cakeshop.InscribeTime / 4
@@ -60,6 +45,7 @@ func BenchmarkVariable(b *testing.B) {
 
 func BenchmarkVariableBuffers(b *testing.B) {
 	cakeshop := defaults
+	cakeshop.Verbose = testing.Verbose()
 	cakeshop.BakeStdDev = cakeshop.BakeTime / 4
 	cakeshop.IceStdDev = cakeshop.IceTime / 4
 	cakeshop.InscribeStdDev = cakeshop.InscribeTime / 4
@@ -70,12 +56,14 @@ func BenchmarkVariableBuffers(b *testing.B) {
 
 func BenchmarkSlowIcing(b *testing.B) {
 	cakeshop := defaults
+	cakeshop.Verbose = testing.Verbose()
 	cakeshop.IceTime = 50 * time.Millisecond
 	cakeshop.Work(b.N)
 }
 
 func BenchmarkSlowIcingManyIcers(b *testing.B) {
 	cakeshop := defaults
+	cakeshop.Verbose = testing.Verbose()
 	cakeshop.IceTime = 50 * time.Millisecond
 	cakeshop.NumIcers = 5
 	cakeshop.Work(b.N)
